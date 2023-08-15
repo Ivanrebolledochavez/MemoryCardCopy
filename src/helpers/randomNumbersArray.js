799e56a9b7cd046a6890dc61b866efdcd65d95ae
Ivan <ivannissimrch@gmail.com>
2023-08-14 21:52:42 -0400

    on randomNumbersArray set maxRandomNumber to 820 to generate numbers from 1 to 820

--- src/helpers/randomNumbersArray.js
+++ src/helpers/randomNumbersArray.js
@@ -1,7 +1,7 @@
 const randomNumbersArray = () => {
   const randomNumberSet = new Set();
   const arrayLength = 5;
-  const maxRandomNumber = 48;
+  const maxRandomNumber = 820;
 
   while (randomNumberSet.size < arrayLength) {
     const randomNumber = Math.floor(Math.random() * maxRandomNumber) + 1;
